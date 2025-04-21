3209e3d38d6679a18879c5b724707d23edfcafab
ogawadl2 <keitaogawano@gmail.com>
2025-04-21 16:51:09 +0900

    update: Moco用の変換を追加

---
+++ src/utils/transform/moco_transforms.py
@@ -0,0 +1,31 @@
+from torchvision import transforms
+
+class MoCoTransform:
+    def __init__(self, n_views: int = 2):
+        """SimCLRのデータ拡張
+        
+        Args:
+            n_views (int): 生成する拡張画像の数（デフォルト: 2）
+        """
+        self.n_views = n_views
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(32, scale=(0.2, 1.0)), # transforms.RandomResizedCrop(32)
+            transforms.RandomGrayscale(p=0.2),
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize([0.4914, 0.4822, 0.4465],
+                                [0.2470, 0.2435, 0.2616]) # CIFAR-10の平均と標準偏差
+        ])
+
+    
+    def __call__(self, x):
+        """入力画像にデータ拡張を適用
+        
+        Args:
+            x: 入力画像
+            
+        Returns:
+            tuple: n_views個の拡張画像のタプル
+        """
+        return tuple(self.transform(x) for _ in range(self.n_views))
--- src/utils/transform_dict.py
+++ src/utils/transform_dict.py
@@ -1,8 +1,9 @@
 from typing import Dict, Any
 from .transform.simclr_transforms import SimCLRTransform
 from .transform.cutout_transforms import CutOut
-
+from .transform.moco_transforms import MoCoTransform
 TRANSFORM_MAP: Dict[str, Any] = {
     "SimCLRTransform": SimCLRTransform,
     "CutOut": CutOut,
+    "MoCoTransform": MoCoTransform,
 }
