b655737d674716104c0c2d7d16db4eeb0f67ba76
ogawadl <keitaogawano@gmail.com>
2025-04-23 18:59:57 +0900

    update: HSSimCLRTransformerにパラメータを追加．

--- src/utils/transform/hs_transforms.py
+++ src/utils/transform/hs_transforms.py
@@ -2,13 +2,39 @@ from torchvision import transforms
 from .hs_colorjitter import RandomHSColorJitter, RandomHSGrayscale
 
 class HSSimCLRTransform:
-    def __init__(self, n_views: int = 2):
+    def __init__(self, n_views: int = 2, hs_colorjitter: bool = True, gaussian_blur: bool = True, instance_normalization: bool = False):
         """SimCLRのデータ拡張
         
         Args:
             n_views (int): 生成する拡張画像の数（デフォルト: 2）
         """
         self.n_views = n_views
+        transform_list = [
+            transforms.RandomResizedCrop(64, scale=(0.2, 1.0)), # transforms.RandomResizedCrop(32)
+            transforms.RandomHorizontalFlip(p=0.5),
+        ]
+        if hs_colorjitter:
+            transform_list.append(
+                transforms.RandomApply([
+                    RandomHSColorJitter(0.4, 0.4, 0.4, 0.1)
+                ], p=0.8)
+            )
+        transform_list.append(
+            transforms.RandomApply([
+                RandomHSGrayscale()
+            ], p=0.2),
+        )
+        if gaussian_blur:
+            transform_list.append(
+                transforms.GaussianBlur(kernel_size=23, sigma=(0.1, 2.0)),
+            )
+        if instance_normalization:
+            transform_list.append(
+                transforms.Lambda(lambda x: (x - x.view(x.size(0), -1).mean(dim=1, keepdim=True).view(x.size(0), 1, 1)) / 
+                                  (x.view(x.size(0), -1).std(dim=1, keepdim=True).view(x.size(0), 1, 1) + 1e-5))
+            )
+            
+        
         self.transform = transforms.Compose([
             transforms.RandomResizedCrop(64, scale=(0.2, 1.0)), # transforms.RandomResizedCrop(32)
             transforms.RandomHorizontalFlip(p=0.5),
